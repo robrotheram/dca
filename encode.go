483e009827d33d318c0969679a700695144aef68
robrotheram <robrotheram@gmail.com>
2022-10-26 19:57:13 +0100

    Fix recconnect issue for youtube live streams

--- encode.go
+++ encode.go
@@ -226,7 +226,8 @@ func (e *EncodeSession) run() {
 	if !regularFile {
 		args = append([]string{
 			"-reconnect", "1",
-			"-reconnect_at_eof", "1",
+			"-reconnect_on_network_error", "1",
+			"-reconnect_on_http_error", "1",
 			"-reconnect_streamed", "1",
 			"-reconnect_delay_max", "2",
 		}, args...)
