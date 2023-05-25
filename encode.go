5b42efd6be2ce07e186cee39dbdd6b33f3ad149e
robrotheram <robrotheram@gmail.com>
2023-05-25 21:07:13 +0100

    Fix YT live streams

--- encode.go
+++ encode.go
@@ -229,7 +229,7 @@ func (e *EncodeSession) run() {
 	if !regularFile {
 		args = append([]string{
 			"-reconnect", "1",
-			"-reconnect_at_eof", "1",
+			// "-reconnect_at_eof", "1", breaks YT live streams
 			"-reconnect_streamed", "1",
 			"-reconnect_delay_max", "5",
 		}, args...)
